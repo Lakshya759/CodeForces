230ee4770f5b18a61236592a3b39339246779081
Lakshya759 <lakshyasri759@gmail.com>
2026-08-19 19:38:36 +0530

    Add Greetings [F] from Codeforces

--- 1915/F - Greetings/solution.cpp
+++ 1915/F - Greetings/solution.cpp
@@ -14,13 +14,7 @@
 using namespace std;
 using namespace __gnu_pbds;
  
-typedef tree<
-    int,
-    null_type,
-    less<int>,
-    rb_tree_tag,
-    tree_order_statistics_node_update
-> ordered_set;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 int main(){
 	int t;cin>>t;
 	while(t--){
